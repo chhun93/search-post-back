4cb9f956dd78cf42e646b7a20871b61e50356e6d
unknown <chhun3830@gmail.com>
2021-11-29 21:12:53 +0900

    임시서버

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3030;
 const server = http.createServer((req, res) => {
   const splitUrl = req.url.split("/");
 
-  if (1 < splitUrl.length) res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   switch (true) {
     case "/" === req.url:
       res.statusCode = 200;
@@ -13,14 +13,38 @@ const server = http.createServer((req, res) => {
       break;
     case 2 === splitUrl.length && "lists" === splitUrl[1]:
       res.statusCode = 200;
-      res.end(`GET lists`);
+      setTimeout(() => {
+        res.end(`{"GET": "URL LISTS"}`);
+      }, 5000);
       break;
     case 3 === splitUrl.length:
-      res.statusCode = 200;
-      console.log(kPostOffice.getTrack(splitUrl[2]));
-      setTimeout(() => {
-        res.end(`{"'GET'": "${splitUrl[2]}"}`);
-      }, 5000);
+      res.writeHead(200, { "Content-Type": "application/json" });
+
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          let test_Json;
+          if (splitUrl[2] === "1234") {
+            let testJsonArray = [];
+            testJsonArray.push({ key: "0", id: "cu", state: "READY" });
+            testJsonArray.push({ key: "1", id: "우체국", state: "READY" });
+            
+            test_Json = testJsonArray;
+          } else if (0 < splitUrl[2].trim().length) {
+            test_Json = [];
+          } else reject("error");
+          
+          res.write(JSON.stringify(test_Json));
+          resolve(test_Json);
+        }, 1500);
+      })
+        .then((response) => {
+          console.log(response);
+          return res.end();
+        })
+        .catch((err) => {
+          return err;
+        });
+
       break;
     default:
       res.statusCode = 404;
@@ -30,3 +54,4 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(process.env.PORT || PORT, () => console.log("Server is opened."));
+
