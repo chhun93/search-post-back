4805d95d4f8ba463a5ffb3eae06b024ff2dc01dd
lee-jeong-geun <ljk0411jg@naver.com>
2021-12-19 16:32:52 +0900

    delete Service Annotation

--- java/src/main/java/org/post/springboot/service/ApiService.java
+++ java/src/main/java/org/post/springboot/service/ApiService.java
@@ -1,11 +1,7 @@
 package org.post.springboot.service;
 
-import org.springframework.stereotype.Service;
-
 import java.util.Map;
 
-@Service
 public interface ApiService {
-
     Map<String, Object> execute(String number);
 }
