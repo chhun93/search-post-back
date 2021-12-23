a91c9c672b2997371ade8dd00cabf312185382a9
lee-jeong-geun <ljk0411jg@naver.com>
2021-12-24 00:17:28 +0900

    move directory

--- src/main/java/org/post/springboot/web/PostApiController.java
+++ src/main/java/org/post/springboot/web/PostApiController.java
@@ -1,14 +1,11 @@
 package org.post.springboot.web;
 
-import io.swagger.annotations.Api;
-import io.swagger.models.auth.In;
 import org.post.springboot.service.ApiService;
 import org.post.springboot.service.EpostService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
