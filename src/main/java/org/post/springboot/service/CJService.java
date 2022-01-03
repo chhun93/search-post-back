0f6430c083cdab48fced1d45301323fad470b895
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-03 22:50:34 +0900

    CJ 서비스 생성

---
+++ src/main/java/org/post/springboot/service/CJService.java
@@ -0,0 +1,13 @@
+package org.post.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class CJService implements ApiService{
+    @Override
+    public Map<String, Object> execute(String number) {
+        return null;
+    }
+}
