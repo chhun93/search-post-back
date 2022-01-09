24b8789e27ad444b1d4cbf32b3d70f540538c2d4
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-09 19:42:05 +0900

    getCSRF 추가

--- src/main/java/org/post/springboot/service/CJService.java
+++ src/main/java/org/post/springboot/service/CJService.java
@@ -1,13 +1,44 @@
 package org.post.springboot.service;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
-public class CJService implements ApiService{
+public class CJService implements ApiService {
+
+    private static final String URL = "https://www.cjlogistics.com/ko/tool/parcel/tracking";
+
+    public String getCSRF(String body) {
+        Document doc = Jsoup.parse(body);
+        List<Element> list = doc.getElementsByAttributeValue("name", "_csrf");
+        String csrf = "";
+
+        if (!list.isEmpty()) {
+            csrf = list.get(0).attr("value");
+        }
+        return csrf;
+    }
+
     @Override
     public Map<String, Object> execute(String number) {
-        return null;
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, Object> result = new HashMap<>();
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(URL, String.class);
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            String csrf = getCSRF(responseEntity.getBody());
+        } else {
+            result.put("result", null);
+        }
+        return result;
     }
 }
