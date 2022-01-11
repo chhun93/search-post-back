7a4805825870e5db6524ec5244582fd5e5f0b749
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-11 23:29:59 +0900

    운송장 조회 결과 dto 추가

---
+++ src/main/java/org/post/springboot/dto/ParcelDto.java
@@ -0,0 +1,30 @@
+package org.post.springboot.dto;
+
+import java.util.List;
+
+public class ParcelDto {
+
+    private String companyName;
+
+    private List<ParcelDetailDto> result;
+
+    public ParcelDto() {
+
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public List<ParcelDetailDto> getResult() {
+        return result;
+    }
+
+    public void setResult(List<ParcelDetailDto> result) {
+        this.result = result;
+    }
+}
