78f129e1ed7688141d15239303fdcd2722138cea
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-11 23:32:17 +0900

    운송장 조회 결과 디테일 dto 추가

---
+++ src/main/java/org/post/springboot/dto/ParcelDetailDto.java
@@ -0,0 +1,48 @@
+package org.post.springboot.dto;
+
+public class ParcelDetailDto {
+
+    private String date;
+
+    private String time;
+
+    private String position;
+
+    private String state;
+
+    public ParcelDetailDto() {
+
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
