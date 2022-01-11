3622f41dd39ab4b4e0931e5fdd9f375cba718ad7
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-11 23:36:14 +0900

    Map -> dto로 수정

--- src/main/java/org/post/springboot/service/EpostService.java
+++ src/main/java/org/post/springboot/service/EpostService.java
@@ -4,24 +4,29 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.post.springboot.dto.ParcelDetailDto;
+import org.post.springboot.dto.ParcelDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class EpostService implements ApiService {
 
     private static final String URL = "https://service.epost.go.kr/trace.RetrieveDomRigiTraceList.comm?sid1={sid1}";
 
-    public List<Map<String, String>> getProcessList(String body) {
+    public List<ParcelDetailDto> getProcessList(String body) {
         if (body == null) {
             return null;
         }
 
-        List<Map<String, String>> resultList = new ArrayList<>();
+        List<ParcelDetailDto> resultList = new ArrayList<>();
 
         try {
             Document doc = Jsoup.parse(body);
@@ -30,20 +35,20 @@ public class EpostService implements ApiService {
             Elements trList = tbody.get(0).getElementsByTag("tr");
 
             for (Element tr : trList) {
-                Map<String, String> resultMap = new HashMap<>();
+                ParcelDetailDto resultMap = new ParcelDetailDto();
                 Elements tdList = tr.getElementsByTag("td");
-                resultMap.put("date", tdList.get(0).text());
-                resultMap.put("time", tdList.get(1).text());
+                resultMap.setDate(tdList.get(0).text());
+                resultMap.setTime(tdList.get(1).text());
 
                 if (tdList.get(2).getElementsByTag("span").isEmpty()) {
-                    resultMap.put("position", tdList.get(2).getElementsByTag("a").text());
+                    resultMap.setPosition(tdList.get(2).getElementsByTag("a").text());
                 } else {
-                    resultMap.put("position", tdList.get(2).getElementsByTag("span").text());
+                    resultMap.setPosition(tdList.get(2).getElementsByTag("span").text());
                 }
                 if (tdList.get(3).text().contains(" ")) {
-                    resultMap.put("state", tdList.get(3).text().substring(0, tdList.get(3).text().indexOf(" ")));
+                    resultMap.setState(tdList.get(3).text().substring(0, tdList.get(3).text().indexOf(" ")));
                 } else {
-                    resultMap.put("state", tdList.get(3).text());
+                    resultMap.setState(tdList.get(3).text());
                 }
                 resultList.add(resultMap);
             }
@@ -54,19 +59,17 @@ public class EpostService implements ApiService {
     }
 
     @Override
-    public Map<String, Object> execute(String number) {
+    public ParcelDto execute(String number) {
         RestTemplate restTemplate = new RestTemplate();
         Map<String, String> params = new HashMap<>();
-        Map<String, Object> result = new HashMap<>();
+        ParcelDto result = new ParcelDto();
 
         params.put("sid1", number);
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(URL, String.class, params);
 
-        result.put("companyName", "epost");
+        result.setCompanyName("epost");
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            result.put("result", getProcessList(responseEntity.getBody()));
-        } else {
-            result.put("result", null);
+            result.setResult(getProcessList(responseEntity.getBody()));
         }
         return result;
     }
