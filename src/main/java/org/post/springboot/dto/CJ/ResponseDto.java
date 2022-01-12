d78db91a99c599062378ddf3cee9e2b05b498469
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-13 00:36:21 +0900

    ResponseDto 추가

---
+++ src/main/java/org/post/springboot/dto/CJ/ResponseDto.java
@@ -0,0 +1,10 @@
+package org.post.springboot.dto.CJ;
+
+public class ResponseDto {
+
+    private ParcelDetailResultMap parcelDetailResultMap;
+
+    public ParcelDetailResultMap getParcelDetailResultMap() {
+        return parcelDetailResultMap;
+    }
+}
