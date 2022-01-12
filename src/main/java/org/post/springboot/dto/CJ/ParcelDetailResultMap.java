c2d88f4a32c2985d84f0b407528b6208be0aa83c
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-13 00:36:44 +0900

    ParcelDetailResultMap 추가

---
+++ src/main/java/org/post/springboot/dto/CJ/ParcelDetailResultMap.java
@@ -0,0 +1,12 @@
+package org.post.springboot.dto.CJ;
+
+import java.util.List;
+
+public class ParcelDetailResultMap {
+
+    private List<Result> resultList;
+
+    public List<Result> getResultList() {
+        return resultList;
+    }
+}
