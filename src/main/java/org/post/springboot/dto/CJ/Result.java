bb6893ee7d5b0aca3aeb02f5a4e767968e58b374
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-13 00:39:38 +0900

    Result 추가

---
+++ src/main/java/org/post/springboot/dto/CJ/Result.java
@@ -0,0 +1,22 @@
+package org.post.springboot.dto.CJ;
+
+public class Result {
+
+    private String dTime;
+
+    private String regBranNm;
+
+    private String scanNm;
+
+    public String getdTime() {
+        return dTime;
+    }
+
+    public String getRegBranNm() {
+        return regBranNm;
+    }
+
+    public String getScanNm() {
+        return scanNm;
+    }
+}
