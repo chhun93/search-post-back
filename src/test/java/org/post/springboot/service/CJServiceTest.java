bd82414f10eae7792758a37ba7980d279b8548ed
lee-jeong-geun <ljk0411jg@naver.com>
2022-01-17 01:19:57 +0900

    JSON 값 파싱 테스트 추가

--- src/test/java/org/post/springboot/service/CJServiceTest.java
+++ src/test/java/org/post/springboot/service/CJServiceTest.java
@@ -2,6 +2,7 @@ package org.post.springboot.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.post.springboot.dto.ParcelDetailDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -96,4 +98,22 @@ public class CJServiceTest {
         //then
         assertThat(result).isNotNull();
     }
+
+    @Test
+    public void JSON_올바른_값_파싱() {
+        //given
+        String body = "{\"parcelResultMap\":{\"resultList\":[{\"invcNo\":\"553825406594\",\"sendrNm\":\"교**\",\"qty\":\"1\",\"itemNm\":\"도서\",\"rcvrNm\":\"이**\",\"rgmailNo\":\"\",\"oriTrspbillnum\":\"\",\"rtnTrspbillnum\":\"\",\"nsDlvNm\":\"91\"}],\"paramInvcNo\":\"553825406594\"}," +
+                "\"parcelDetailResultMap\":{\"resultList\":[{\"nsDlvNm\":\"\",\"crgNm\":\"보내시는 고객님으로부터 상품을 인수받았습니다\",\"crgSt\":\"11\",\"dTime\":\"2021-12-28 11:05:25.0\",\"empImgNm\":\"EMP_IMG_NM\",\"regBranId\":\"8257\",\"regBranNm\":\"[집화]케이비\",\"scanNm\":\"집화처리\"}], \"paramInvcNo\":\"553825406594\"}}";
+
+        //when
+        List<ParcelDetailDto> list = service.getProcessList(body);
+
+        //then
+        assertThat(list.size()).isEqualTo(1);
+        assertThat(list.get(0).getDate().toString()).isEqualTo("2021-12-28");
+        assertThat(list.get(0).getTime().format(DateTimeFormatter.ofPattern("HH:mm"))).isEqualTo("11:05");
+        assertThat(list.get(0).getPosition()).isEqualTo("[집화]케이비");
+        assertThat(list.get(0).getState()).isEqualTo("집화처리");
+    }
+
 }
